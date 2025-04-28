630c3072a36b5b484b9f94dec95d2bf17aa5821c
Rey418 <falxspeed@gmail.com>
2025-04-28 15:29:38 -0400

    Delete easyStats directory

--- easyStats/R/Example.R
+++
@@ -1,9 +0,0 @@
-install.packages("easystats")
-library(easyStats)
-install.packages("normalize")
-library(normalize)
-# Summary stats
-summary_stats(c(1, 2, 3, 4, 5))
-
-# Normalize
-normalize(c(10, 20, 30))
